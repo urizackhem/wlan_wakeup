342f1301fe58d7232d2b604de2e845b9e739accd
Uri ZACKHEM <urizackhem@gmail.com>
2017-06-29 09:01:50 +0300

    Update wlan_wakeup.pl
    Mainly comments.

--- wlan_wakeup.pl
+++ wlan_wakeup.pl
@@ -2,28 +2,30 @@
 
 use strict;
 use warnings;
-
+# Install this nice module.
 use Net::Ping;
-
+# The IP address of your router. Can be different, like 10.0.0.1
 my $host = "192.168.1.1";
-
+# Instanciate
 my $p = Net::Ping->new();
 if ($p->ping($host))
-{
+{       # Everything's hunky dory.
 	print "Network's ON\n";
 	$p->close();
 }else{
 	$p->close();
+	# Try to shut down the wifi and revive it.
 	print "Network's DOWN\n";
 	my $wlan = "wlan0";
 	`ifdown $wlan`;
 	sleep(5);
 	`ifup --force $wlan`;
 	if (not ($p->ping($host)) )
-	{	
+	{	# Emergency powers...
 		`sudo shutdown -h now`;
 	}
 	$p->close();
+	print "Network's UP now\n";
 }
 
 print "Finished\n";
